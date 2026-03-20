fda85a0570550fabd56ffe168bb23d20e282bfa5
Millie <millieamcvey@gmail.com>
2026-03-20 12:42:55 +0000

    Column Organjsation

--- readd.py
+++ readd.py
@@ -1,10 +1,14 @@
+# imports
 import pandas as pd
 
+# 
 data = pd.read_csv('dataset.csv')
 
+# column selection
 column1 = input("enter column name:")
 column2 = input("enter column name:")
 
+# input validation
 if column1 in data.columns and column2 in data.columns:
     print(data[["track_name", column1, column2]])
 else:
