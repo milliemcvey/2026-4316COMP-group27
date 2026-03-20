8f53311fb50d23fd1e5ad58aa19c8fcee98ffc55
daisyfar <djfarrow@outlook.com>
2026-03-20 12:40:11 +0000

    comment

--- readd.py
+++ readd.py
@@ -3,9 +3,11 @@ import pandas as pd
 data = pd.read_csv('dataset.csv')
 
 column1 = input("enter column name:")
+filt1 = input("enter value:")
 column2 = input("enter column name:")
 
+#shows selected columns
 if column1 in data.columns and column2 in data.columns:
-    print(data[["track_name", column1, column2]])
+    print(data[[column1, column2]])
 else:
     print("not column")
