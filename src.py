0449b25a86c6d35e25238837046f6b601b286660
adelaide161106 <adelaidej2006@outlook.com>
2026-04-12 11:46:59 +0100

    avg calculation

--- src.py
+++ src.py
@@ -83,7 +83,7 @@ print(f"Average of {column1}: {avg1:.2f}") #gives results
 print(f"Average of {column2}: {avg2:.2f}")
 
 except Exception as e:
-      print("Error calclulating the avergages:", e)
+      print("Error calclulating the average:", e)
 
 
     #add in additional functionality (create another enquiry) here
