bc65c4956b49093d17212ab0eed0d9de0d42bd9b
Millie <millieamcvey@gmail.com>
2026-04-22 23:34:55 +0100

    Update MillieMcVey-visualisation.py

--- MillieMcVey-visualisation/MillieMcVey-visualisation.py
+++ MillieMcVey-visualisation/MillieMcVey-visualisation.py
@@ -21,11 +21,9 @@ def genrePopularityQuery(data):
     plt.title("Is there a 'most popular genre'?")
     plt.xlabel("Genre", fontsize =12)
     plt.ylabel("Average Popularity", fontsize=12)
-    
     plt.tight_layout()
     plt.show()
     
-
 # Investigation: Does the key of a song affect the energy?
 # Columns: Key & Energy
 
@@ -54,7 +52,6 @@ def keyEnergyQuery(data):
     plt.tight_layout()
     plt.show()
     
-
 # Calling both functions:
 genrePopularityQuery(data)
 keyEnergyQuery(data)
